3983d880bd7a47fdca52a0328f8ffc0e81dc0eb1
Felipe <felipe.araujo@hubfintech.com.br>
2020-03-16 18:26:56 -0300

    added route wrapper for private and/or public routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import RouteWrapper from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Page404 from '../pages/Page404';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path='/' component={SignIn} />
-      <Route path='/register' component={SignUp} />
-      <Route path='/dashboard' component={Dashboard} />
-      <Route path='/profile' component={Profile} />
+      <RouteWrapper exact path='/' component={SignIn} />
+      <RouteWrapper path='/register' component={SignUp} />
+      <RouteWrapper path='/dashboard' component={Dashboard} isPrivate />
+      <RouteWrapper path='/profile' component={Profile} isPrivate />
+      <RouteWrapper path='*' component={Page404} />
     </Switch>
   );
 }
