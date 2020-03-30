6c7f9a232101cf5cb85c1be054c342d3f84fea7d
Felipe <felipe1180@hotmail.com>
2020-03-30 10:57:51 -0300

    root imports

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import './config/ReactotronConfig';
 import history from './services/history';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyles />
       <Routes />
     </Router>
   );
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
   isPrivate = false,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
+
   if (!signed && isPrivate) {
     return <Redirect to='/' />;
   }
   if (signed && !isPrivate) {
     return <Redirect to='dashboard' />;
   }
+  const Layout = signed ? AuthLayout : DefaultLayout;
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...rest} component={Component} />;
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
